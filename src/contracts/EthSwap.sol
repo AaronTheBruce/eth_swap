661e8a2a037390f51801541d72e319b997e36700
Aaron Bruce <aaronbruce555@gmail.com>
2020-12-04 16:17:55 -0500

    Add sellTokens and test

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -7,8 +7,15 @@ contract EthSwap {
   string public name = "EthSwap Instant Exchange"; // state var
   Token public token; // state var
   uint public rate = 100;
-  
+
   event TokenPurchased(
+    address account,
+    address token,
+    uint amount,
+    uint rate
+  );
+
+  event TokensSold(
     address account,
     address token,
     uint amount,
@@ -23,9 +30,32 @@ contract EthSwap {
   function buyTokens() public payable { // payable allows the sending of crypto
     // Calculate the number of tokens to buy
     uint tokenAmount = msg.value * rate;
+
+    // require is a boolean check to verify a true statement to continue with rest of block
+    require(token.balanceOf(address(this)) >= tokenAmount); // EthSwap's balance must be greater than or equal to the requested amount
+
+    // Trasnfer tokens to the user
     token.transfer(msg.sender, tokenAmount);
 
     // Emit an event that tokens were purchased
     emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+  }
+
+  function sellTokens(uint _amount) public {
+    // User cannot sell more tokens than they have
+    require(token.balanceOf(msg.sender) >= _amount);
+
+    // Calculate the amount of Ether to redeem
+    uint etherAmount = _amount / rate;
+
+    // Require that EthSwap has enough Ether
+    require(address(this).balance >= etherAmount);
+
+    // Perform Sale
+    token.transferFrom(msg.sender, address(this), _amount);
+    msg.sender.transfer(etherAmount);
+
+    // Emit and Event
+    emit TokensSold(msg.sender, address(token), _amount, rate);
   }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,5 +1,4 @@
 const { assert } = require('chai');
-const { default: Web3 } = require('web3');
 
 /* eslint-disable no-undef */
 const Token = artifacts.require('Token');
@@ -9,7 +8,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-function tokens(n){ // converts tokens into their 'Cents' equivalent. Allows us to control visually numbers of tokens
+function tokens(n) { // converts tokens into their 'Cents' equivalent. Allows us to control visually numbers of tokens
   return web3.utils.toWei(n, "ether");
 }
 
@@ -43,8 +42,8 @@ contract('EthSwap', ([deployer, investor]) => {
 
   describe('buy tokens', async () => {
     let result;
-    before(async() => {
-      result = await ethSwap.buyTokens({from: investor, value: web3.utils.toWei('1', 'ether')});
+    before(async () => {
+      result = await ethSwap.buyTokens({ from: investor, value: web3.utils.toWei('1', 'ether') });
     })
     it('Allows user to instantly purchase tokens from ethSwap for a fixed price', async () => {
       let investorBalance = await token.balanceOf(investor);
@@ -61,6 +60,37 @@ contract('EthSwap', ([deployer, investor]) => {
       assert.equal(event.token, token.address);
       assert.equal(event.amount.toString(), tokens('100').toString());
       assert.equal(event.rate.toString(), '100');
+    })
+  })
+
+  describe('sell tokens', async () => {
+    let result;
+
+    before(async () => {
+      // Check investor token balance after purchase
+      await token.approve(ethSwap.address, tokens('100'), { from: investor });
+      result = await ethSwap.sellTokens(tokens('100'), { from: investor });
+    })
+    it('Allows user to instantly sell tokens to ethSwap for a fixed price', async () => {
+      // Check investor token balance after purchase
+      let investorBalance = await token.balanceOf(investor);
+      assert.equal(investorBalance.toString(), tokens('0'));
+
+      // Check ethSwap balance after purchase
+      let ethSwapBalance;
+      ethSwapBalance = await token.balanceOf(ethSwap.address);
+      assert.equal(ethSwapBalance.toString(), tokens('1000000'));
+      ethSwapBalance = await web3.eth.getBalance(ethSwap.address);
+      assert.equal(ethSwapBalance.toString(), tokens('0'));
+
+      // Check logs to ensure event was emitted with correct data
+      const event = result.logs[0].args;
+      assert.equal(event.account, investor);
+      assert.equal(event.token, token.address);
+      assert.equal(event.amount.toString(), tokens('100').toString());
+      assert.equal(event.rate.toString(), '100');
+
+      await ethSwap.sellTokens(tokens('500'), { from: investor }).should.be.rejected;
     })
   })
 
