068bb6f4beb15fc6a09494cebdc6cc7d3df3a18e
Aaron Bruce <aaronbruce555@gmail.com>
2020-12-07 17:24:45 -0500

    Add Loading of Token

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,95 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
+import EthSwap from '../abis/EthSwap.json';
+import Token from '../abis/Token.json';
+import NavBar from './NavBar';
 import './App.css';
 
+/**
+ * Connect Browser To Blockchain : Metamask!
+ * Connect App To Blockchain
+ */
+
 class App extends Component {
+
+  async componentWillMount() {  // renders before DOM is rendered
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+    console.log(window.web3)
+  }
+
+  async loadBlockchainData() {
+    const web3 = await window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({account: accounts[0]});
+
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ethBalance});
+
+
+    // Load Token
+    const networkId = await web3.eth.net.getId();
+    const tokenData = Token.networks[networkId];
+    if(tokenData){
+      const token = new web3.eth.Contract(Token.abi, tokenData.address)
+      this.setState({token});
+      let tokenBalance = await token.methods.balanceOf(this.state.account).call();
+      this.setState({tokenBalance: tokenBalance.toString()})
+    } else {
+      window.alert('Token Contract not deployed to detected network')
+    }
+
+
+    // Load EthSwap
+
+  }
+
+  async loadWeb3() {
+    // pull in ethereum provider from metamask
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      try {
+        // Request account access if needed
+        await window.ethereum.enable();
+        // Acccounts now exposed
+        // web3.eth.sendTransaction({/* ... */});
+      } catch (error) {
+        // User denied account access...
+        console.error(error);
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+      // Acccounts always exposed
+      window.web3.eth.sendTransaction({/* ... */ });
+    }
+    // Non-dapp browsers...
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      account: '',
+      ethBalance: '0',
+      daiToken: {},
+      dappToken: {},
+      tokenFarm: {},
+      daiTokenBalance: '0',
+      dappTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <NavBar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -25,20 +99,8 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Hello World!</h1>
               </div>
             </main>
           </div>
